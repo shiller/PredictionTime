0051face1586b94404514c79a1111d4480c7ab38
shiller <shiller@student-pc3>
2014-03-05 15:59:44 +0100

    calculate median of rt of same sequences
    make for all tools erference dicts for output

--- PredictionTime/PredictionToolExecuter.py
+++ PredictionTime/PredictionToolExecuter.py
@@ -2,13 +2,13 @@
 
 import os
 import subprocess 
+import re
 
 from pyteomics import biolccc
 
-import SSRCalcTest
 import Util
-from lxml.html.builder import PARAM
 import SSRCalcTest
+from lxml.html.builder import PARAM
 
 def factory(tool):
     if tool == "Elude":
@@ -30,47 +30,51 @@ def executeElude(tool):
     general = Util.readConfigFile(Util.TOOL_CONFIG, "General")
     
     cmd = "%(cmd)s" % Util.readConfigFile(Util.TOOL_CONFIG, tool)
-    cmd = (cmd + " -t " + general["trainDirectory"] + paramToValue["trainFile"] + " -e " + general["testDirectory"] + paramToValue["ioFile"] + " -o " + general["outputDirectory"] + paramToValue["ioFile"] + paramToValue["verbose"])
+    cmd = (cmd + " -t " + general["trainDirectory"] + paramToValue["trainFile"] + " -e " + general["testDirectory"] + paramToValue["ioFile"] + " -o " + general["outputDirectory"] + paramToValue["ioFile"] + " " + paramToValue["verbose"])
     p = subprocess.Popen([cmd], stderr=subprocess.PIPE, shell=True)
     out, err = p.communicate()
     ## TODO: wenn fehler, dann print
-#     print out, err
-#     p.wait()
+    print out, err
     
 #     takes output and connect with input rt
-    output_RT = open(Util.TOOLS_OUTPUT+"Elude.txt-v", 'r')
-    content = output_RT.readlines()
+    output_RT_fh = open(Util.TOOLS_OUTPUT+"Elude.txt", 'r')
+    content = output_RT_fh.readlines()
+    output_RT_fh.close()
+    
     #remove header
     output_RT = content[3:]
     output_set = {}
     
     for seq in output_RT:
-        print seq
-        sseq = seq.split("\t")[0]
-        sseq= sseq.strip()
+        unimod = seq.split("\t")[0]
+        unimod= unimod.strip()
         rt = seq.split("\t")[1]
         rt = rt.strip()
-        output_set[seq] = rt
-        print output_set[seq], " = ", sseq, rt 
+        output_set[unimod] = rt
+
 
     testRef_RT = open(Util.PATH_TO_TMP+"Elude_test_reference.txt", 'r')
     test_set = {}
 #TODO: verknuepfe mir meine zwei sachen  modseq : rt fuer output
-    for seq in testRef_RT:
-#   #key=toolinputseq; value=modSeq
-        sseq = seq.split('\t')[1]
+    for line in testRef_RT:
+        sseq = line.split('\t')[1]
         sseq = sseq.strip()
-        modSeq = seq.split('\t')[0]
-        modSeq = modSeq.strip()
-        test_set[modSeq] = sseq
+        unimod = line.split('\t')[0] #key: toolinput
+        unimod = unimod.strip()
+        test_set[unimod] = sseq #returns value
     testRef_RT.close()    
 
-    # write Tooloutput    
+    # write Tooloutputfile    
     elude = open(Util.TOOLS_OUTPUT+"Elude.txt", 'w')
-    for seq in output_set.keys():
-        elude.write("\t".join((str(test_set[seq]), str(output_set[seq])))) #Ausgabe: value testRef_RT = modSeq mit phosphos, RT Zeit  
+    for key in output_set.keys():
+        #rt = output_set[key]
+        seq = test_set[key]
+        seq = "."+ seq + "." # for PredictionToolReader.getSequence
+        key = key.strip()
+        elude.write("\t".join((  str(seq), str(output_set[key])) )) #Ausgabe: value testRef_RT = modSeq mit phosphos, RT Zeit  
         elude.write("\n")
-    elude.close()
+    elude.close(), #os.unlink(Util.TOOLS_OUTPUT+"Elude.txt") #delete output because of Reference_R.mat
+    
     
  
 def executeSSRcalc(tool):  
@@ -137,6 +141,49 @@ def executeBioLCCC(tool):
             sequence = fileLine.strip() 
             retentionTime = biolccc.calculateRT(sequence, chemBasis, myChromoConditions)
             fobjOut.write(sequence + "\t" + str(retentionTime) + "\n")
+    fobjOut.close()
+            
+#             #     take output and connect with input rt
+    output_RT_fh = open(Util.TOOLS_OUTPUT+"BioLCCC.txt", 'r')
+    content = output_RT_fh.readlines()
+    output_RT = content
+    output_RT_fh.close()
+    output_set = {}
+     
+    for seq in output_RT:
+        seq = seq.strip()
+        data = re.split('\t', seq)
+        if len(data) != 2:
+            print "ERROR in len data", data
+            exit(-1)
+        unimod = data[0]
+        rt = data[1]
+        output_set[unimod] = rt
+ 
+    testRef_RT = open(Util.PATH_TO_TMP+"BioLCCC_test_reference.txt", 'r')
+    test_set = {}
+# verknuepfe mir meine zwei sachen  modseq : rt fuer output
+    for line in testRef_RT:
+        sseq = line.split('\t')[1]
+        sseq = sseq.strip()
+        unimod = line.split('\t')[0] #key: toolinput
+        unimod = unimod.strip()
+        test_set[unimod] = sseq #returns value
+    testRef_RT.close()    
+ 
+    os.unlink(Util.TOOLS_OUTPUT+"BioLCCC.txt")
+    # write Tooloutput    
+    biolc = open(Util.TOOLS_OUTPUT+"BioLCCC.txt", 'w')
+    for key in output_set.keys():
+        #rt = output_set[key]
+        seq = test_set[key]
+        seq = "-"+ seq + "-" # for PredictionToolReader.getSequence
+        key = key.strip()
+        biolc.write("\t".join((  str(seq), str(output_set[key])) )) #Ausgabe: value testRef_RT = modSeq mit phosphos, RT Zeit  
+        biolc.write("\n")
+            
+            
+            
+    
 
-    fobjOut.close() 
     
--- PredictionTime/Run.py
+++ PredictionTime/Run.py
@@ -6,32 +6,33 @@ import PredictionToolWriter
 import PredictionToolReader
 import FileReader
 
- 
- 
-Util.cleanUp()
-# create RMatrix with matches of modified phosphosites 
-FileReader.modifyPeptides()
-#  read matches (modified) or all files in dir
-allRefMat = PredictionToolWriter.readReference(Util.PATH_TO_TRAIN_REF)
- 
-#  TODO: take MATCHES.txt and create train & test files if PATH_TO_TEST+empty)
-#  create random_trainset and testset with RT
-PredictionToolWriter.createSampleFile(Util.PATH_TO_TRAIN_REF)
- 
- 
-# create matrix out of file; HEADER! oder mit writeMatrix, dann header weg
-trainMatrix = PredictionToolWriter.readReference(Util.PATH_TRAIN_TMP)   
-trainMatrix = PredictionToolReader.modifyRetentionTimes(trainMatrix)
- 
-tools = ["Elude", "SSRCalc", "BioLCCC"]
-for tool in tools:
-    PredictionToolWriter.writeTrainInput(trainMatrix, Util.PATH_TO_TMP, tool) #neue spalte einfuegen mit ID als sseq mit RT (toolinputseq zeigt auf modifi.Seq)
-    #problem: ssrcalc nur unmodifizierte seq, rest mit -1
-     
+    
+       
+# Util.cleanUp()
+# # create RMatrix with matches of modified phosphosites 
+# FileReader.modifyPeptides()
+# #  read matches (modified) or all files in dir
+# allRefMat = PredictionToolWriter.readReference(Util.PATH_TO_TRAIN_REF)
+#         
+# #  TODO: take MATCHES.txt and create train & test files if PATH_TO_TEST+empty)
+# #  create random_trainset and testset with RT
+# PredictionToolWriter.createSampleFile(Util.PATH_TO_TRAIN_REF)
+#       
+#       
+# # create matrix out of file; HEADER! oder mit writeMatrix, dann header weg
+# trainMatrix = PredictionToolWriter.readReference(Util.PATH_TRAIN_TMP)   
+# trainMatrix = PredictionToolReader.modifyRetentionTimes(trainMatrix)
+#       
+# tools = ["Elude", "SSRCalc", "BioLCCC"]
+# for tool in tools:
+#     PredictionToolWriter.writeTrainInput(trainMatrix, Util.PATH_TO_TMP, tool) #neue spalte einfuegen mit ID als sseq mit RT (toolinputseq zeigt auf modifi.Seq)
+#     #problem: ssrcalc nur unmodifizierte seq, rest mit -1
+
 # create toolinput for test; HEADER!
 testMatrix = PredictionToolWriter.readReference(Util.PATH_TO_TEST_REF)
 testMatrix = PredictionToolReader.modifyRetentionTimes(testMatrix)
-tools = ["Elude", "SSRCalc", "BioLCCC"]
+#tools = ["Elude", "SSRCalc", "BioLCCC"]
+tools = ["Elude"]
 for tool in tools:
     PredictionToolWriter.writeToolInput(testMatrix, Util.TOOLS_INPUT, tool)
     PredictionToolExecuter.factory(tool)
